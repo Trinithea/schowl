8415e4383c05bb6f495de06ea1047a855369b2f0
Eliáš Cizl <cizl.elias@gmail.com>
2020-02-10 11:52:43 +0100

    Added about page holder (not content)

--- Schowl/app/src/main/java/cz/cizlmazna/schowl/MainActivity.kt
+++ Schowl/app/src/main/java/cz/cizlmazna/schowl/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_settings
+                R.id.nav_settings, R.id.nav_about
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ Schowl/app/src/main/java/cz/cizlmazna/schowl/ui/about/AboutFragment.kt
@@ -0,0 +1,22 @@
+package cz.cizlmazna.schowl.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import cz.cizlmazna.schowl.R
+import cz.cizlmazna.schowl.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentAboutBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+
+        return binding.root
+    }
+}
